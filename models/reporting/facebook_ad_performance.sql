823d6359299e21b3a8b6370ba6fa1df76aa61d45
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2026-08-28 14:14:38 -0300

    Add account categorization in Facebook ad performance query

--- models/reporting/facebook_ad_performance.sql
+++ models/reporting/facebook_ad_performance.sql
@@ -3,6 +3,9 @@
 )}}
 
 SELECT 
+     CASE WHEN account_id = '594708350991342' THEN 'DTC'
+         WHEN account_id in ('555228837680936') THEN 'Sephora'
+    END AS account,
 campaign_name,
 campaign_id,
 campaign_effective_status,
